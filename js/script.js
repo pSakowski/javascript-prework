9ceea1bac9333f129b3a45994762f4cc054394dd
psakowski <p.sakowski1213@gmail.com>
2022-08-09 13:38:09 +0200

    fix js/script.js

--- js/script.js
+++ js/script.js
@@ -42,34 +42,10 @@ if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
   }
 
-  if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis!');
+  else if(computerMove == playerMove){
+    printMessage ('Remis!');
   }
 
-  if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Ty przegrywasz!');
-  }
-
-  if( computerMove == 'nożyce' && playerMove == 'papier'){
-    printMessage('Ty przegrywasz!');
-  }
-
-  if( computerMove == 'nożyce' && playerMove == 'kamień'){
-    printMessage('Ty wygrywasz!');
-  }
-
-  if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');
-  }
-
-  if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis!');
-  }
-
-  if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Ty przegrywasz!');
-  }
-
-  if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');
+  else {
+    printMessage ('Przegrywasz!');
   }
