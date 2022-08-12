b5f3551f77912e8c8fb6c63a2cc83c0d0ac0cc6d
psakowski <p.sakowski1213@gmail.com>
2022-08-12 19:37:27 +0200

    change let to const

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-function playGame(playerInput){
+{
+  function playGame(playerInput){
 
   clearMessages();
 
@@ -15,17 +16,17 @@ function playGame(playerInput){
     return 'nieznany ruch';
   }
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  const randomNumber = Math.floor(Math.random() * 3 + 1);
 
   console.log('Wylosowana liczba to: ' + randomNumber);
 
-  let computerMove = getMoveName(randomNumber);
+  const computerMove = getMoveName(randomNumber);
 
   printMessage('Mój ruch to: ' + computerMove);
 
   console.log('Gracz wpisał: ' + playerInput);
 
-  let playerMove = getMoveName(playerInput);
+  const playerMove = getMoveName(playerInput);
 
   printMessage('Twój ruch to: ' + playerMove);
 
@@ -59,3 +60,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
 });
+}
